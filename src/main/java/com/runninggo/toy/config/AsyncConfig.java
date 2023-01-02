aeb596a3cb1793964968ec5f51004e0684db9670
minjee <kimmj1106@naver.com>
2023-01-02 17:57:00 +0900

    [comment] AsyncConfig에 주석 추가

--- src/main/java/com/runninggo/toy/config/AsyncConfig.java
+++ src/main/java/com/runninggo/toy/config/AsyncConfig.java
@@ -7,16 +7,17 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
-@Configuration
-@EnableAsync
+@Configuration //Spring 설정 관련 Class에 달아줌. @Component 등록되어 bean으로 등록 될 수 있다.
+@EnableAsync //Spring method에서 비동기 기능을 사용가능하게 활성화 한다.
 public class AsyncConfig {
 
     @Bean
     public Executor customAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(5);
-        executor.setThreadNamePrefix("email-thread");
+        executor.setCorePoolSize(5); //기본 실행 대기하는 Thread의 수
+        executor.setMaxPoolSize(5); //동시 동작하는 최대 Thread의 수
+        executor.setQueueCapacity(50); //MaxPoolSize 초과 요청에서 Thread 생성 요청시,해당 요청을 Queue에 저장하는데 이때 최대 수용 가능한 Queue의 수. Queue에 저장되어있다가 Thread에 자리가 생기면 하나씩 빠져나가 동작
+        executor.setThreadNamePrefix("email-thread"); //생성되는 Thread 접두사 지정
         executor.initialize(); // 꼭 써줘야 한다.
         return executor;
     }
--- src/main/java/com/runninggo/toy/controller/JoinController.java
+++ src/main/java/com/runninggo/toy/controller/JoinController.java
@@ -3,6 +3,8 @@ package com.runninggo.toy.controller;
 import com.runninggo.toy.domain.MemberDto;
 import com.runninggo.toy.service.MemberService;
 import com.runninggo.toy.validator.JoinCkValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.TaskRejectedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequestMapping("/join")
 public class JoinController {
@@ -48,7 +51,11 @@ public class JoinController {
         //유효성 검사를 통과하면 insert 후 페이지 이동
         int result = memberService.insertMember(memberDto);
         if(result == 1){
-            memberService.sendJoinCertificationMail(memberDto); //인증메일 보내기
+//            try { //예외처리 더 고민해보기.
+                memberService.sendJoinCertificationMail(memberDto); //인증메일 보내기
+//            } catch (TaskRejectedException e) {
+//                log.info("TaskRejectedException 발생={}", e.getStackTrace());
+//            }
             return "/member/joinSuccessForm";
         }
         return "/member/joinForm";
